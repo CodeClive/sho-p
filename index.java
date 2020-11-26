dae23799f96c4b66877b504f23d516fdb0327f9a
CodeClive <75058262+CodeClive@users.noreply.github.com>
2020-11-26 16:08:00 +0800

    创建了index.java文件

---
+++ index.java
@@ -0,0 +1,10 @@
+public class Person{
+        public static void main (String[] args){
+                int a = 10;
+                int b = 20;
+                int temp = a + b;
+                System.out.println(temp);
+
+}
+
+}
